c82ef1513a0bdb6b442f8b06d7c4562dbd6d10d1
savitsav <59965809+savitsav@users.noreply.github.com>
2020-01-23 10:03:57 -0500

    my update

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * This class is our first Example class for review  changed
  *
  * @author Paul Bonenfant
- *modified
+ *modified by Savita
  */
 public class StudentList {
 
